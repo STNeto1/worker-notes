adabf1203f25992291b66c419efac2703a64e080
STNeto1 <gfreitasneto18@gmail.com>
2023-11-07 14:46:12 -0300

    pscale 2

--- src/db/connections.ts
+++ src/db/connections.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { env } from "~/env/server";
 
 const connection = connect({
-  url: env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(connection, {
